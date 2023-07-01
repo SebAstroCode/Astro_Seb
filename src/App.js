c2c12e8a04cb0739353850d86ded98804d6a7195
SebAstroCode <sebastrocode@gmail.com>
2023-07-01 23:14:56 +0200

    ajout arborescence 'pages' dans components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './Components/Header';
+import Pages from './Components/Pages';
 import Footer from './Components/Footer';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-         <Header />
-         <Footer />
+        <Header />
+        <Pages />
+        <Footer />
     </div>
   );
 }
---
+++ src/Components/Pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './pages.scss';
+
+const Pages = () => {
+    return(
+        <section className="page_content">test pages</section>
+    )
+}
+export default Pages;
