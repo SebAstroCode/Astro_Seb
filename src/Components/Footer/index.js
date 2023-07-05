e3085885d620b5769fc9349a7f15bcea1dc53a75
SebAstroCode <sebastrocode@gmail.com>
2023-07-05 18:22:57 +0200

    footer css flexbox

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,8 +1,27 @@
 import React from "react";
+import './footer.scss';
 
 const Footer = () => {
+    const now = new Date();
+    // console.log (Date);
+    const year = now.getFullYear();
+    // console.log(year);
     return(
-        <h4 className="footer_title">By AstroSeb</h4>
+        <>
+            <h4 className="footer_title">By AstroSeb &copy; {year}</h4>
+                <div className="footer_box">
+                <ul className="footer_liste">
+                    <li><a href="https://lesia.obspm.fr/">LESIA</a></li>
+                    <li><a href="https://www.observatoiredeparis.psl.eu/">Observatoire de Paris PSL</a></li>
+                    <li><a href="https://cnes.fr/fr">CNES</a></li>
+                </ul>
+                <ul className="footer_liste">
+                    <li><a href="https://lesia.obspm.fr/">LESIA</a></li>
+                    <li><a href="https://www.observatoiredeparis.psl.eu/">Observatoire de Paris PSL</a></li>
+                    <li><a href="https://cnes.fr/fr">CNES</a></li>
+                </ul>
+                </div>
+        </>
     )
 }
 export default Footer;
